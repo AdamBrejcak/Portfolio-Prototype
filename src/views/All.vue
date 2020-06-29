beccd70ed915c578b009b99ae32b6bc5aae88ec4
AdamBrejcak <60700891+AdamBrejcak@users.noreply.github.com>
2020-06-29 10:20:57 +0200

    nejde to

--- src/views/All.vue
+++ src/views/All.vue
@@ -1,28 +1,37 @@
 <template>
   <main>
-    <div>
-      <button @click="downloadFile()"></button>
+    <div class="background" :style="{backgroundImage: computedImg}">
+      <ul>
+        <li v-for="item in items" :key="item.id">{{item.name}}</li>
+      </ul>
+      <button @click="getSection()"></button>
     </div>
   </main>
 </template>
 
 <script>
 // import DB from "../firebaseInit";
-import firebase from "firebase";
 import { getters } from "../store";
 
 export default {
   name: "All",
   computed: {
     ...getters,
+    computedImg() {
+      return this.imgUrl;
+    },
+  },
+  data() {
+    return {
+      items: [],
+      imgSrc: "https://firebasestorage.googleapis.com/v0/b/deafeye-1539950687727.appspot.com/o/Peter_a_Petra_da998bb0-b4a9-11ea-a2ba-a3431d1e8e0b%2Fda998bb1-b4a9-11ea-a2ba-a3431d1e8e0b?alt=media&token=a5579a3d-d5f7-4f1a-b467-a669bed34b97",
+      imgUrl: "url('this.imgSrc')",
+    };
   },
   methods: {
-    downloadFile() {
-      const storage = firebase.storage();
-      // const pathRef = storage.ref("Daniel_a_Monika/7cb80f00-aca5-11ea-903d-bd3d54865a8b.jpeg");
-      const storageRef = storage.refFromURL("gs://deafeye-1539950687727.appspot.com/Daniel_a_Monika/7cb80f00-aca5-11ea-903d-bd3d54865a8b.jpeg");
-      const starsRef = storageRef.child("Daniel_a_Monika/7cb80f00-aca5-11ea-903d-bd3d54865a8b.jpeg");
-      console.log(starsRef.getDownloadURL());
+    getSection() {
+      this.items = this.sections;
+      console.log(this.items);
     },
   },
   mounted() {
@@ -35,5 +44,11 @@ export default {
     width: 50px;
     height: 30px;
     background-color: aqua;
+  }
+
+  .background{
+    width: 100%;
+    height: 1080px;
+
   }
 </style>
