aef3c2c5864fcebbbde49b2024eb942937e3443f
AdamBrejcak <60700891+AdamBrejcak@users.noreply.github.com>
2020-07-18 16:56:15 +0200

    background v2

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,13 @@ module.exports = {
   configureWebpack: {
     devtool: "source-map",
   },
+  // vue.config.js
+  chainWebpack: (config) => {
+    config.module
+      .rule("eslint")
+      .use("eslint-loader")
+      .options({
+        fix: true,
+      });
+  },
 };
